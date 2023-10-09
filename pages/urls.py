5ee2a579e67d2298a10b091fd2d6aeb62f88486b
piet <pprotassow@posteo.de>
2023-10-09 20:13:04 +0200

    templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView #new
+from .views import HomePageView, AboutPageView, homePageView  #new
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'), #new
     path('', HomePageView.as_view(), name='home'),
+    path('home2', homePageView, name='home2'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,25 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['key1'] = 'value1'
+        context['key2'] = 'value2'
+        context['key3'] = ['HELLO', 2, 3, 4, 5]
+        return context
+
+# function based view
+def homePageView(request):
+    print(request.user)
+    context = {
+        'key1': 'value1',
+        'key2': 'value2',
+        'key3': [1, 2, 3, 4, 5],
+    }
+    response = render(request, 'home2.html', context)
+    response.write('<p>Here is the text of my page.</p>')
+    return response
+    # return render(request, 'home2.html', context) #response object
+
 class AboutPageView(TemplateView): #new
     template_name = 'about.html'
